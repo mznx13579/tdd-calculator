f850301da9f9f3f1843ac1455a40025f3b70b5b6
Ompang <mznx13579@naver.com>
2022-01-26 21:59:49 +0900

    Update calculator.spec.js

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -43,14 +43,12 @@ describe('계산기 애플리케이션 테스트', () => {
     cy.get('#total').should('have.text', '1');
   });
 
-// [] AC(All Clear)버튼을 누르면 0으로 초기화 한다.
   it('AC(All Clear)버튼을 누르면 0으로 초기화 한다.', () => {
     cy.get('.digit').contains('3').click();
     cy.get('.modifier').contains('AC').click();
     cy.get('#total').should('have.text', '0');
   });
 
-// [] 숫자는 한번에 최대 3자리 수까지 입력 가능하다.
   it('숫자는 한번에 최대 3자리 수까지 입력 가능하다.', () => {
     cy.get('.digit').contains('1').click();
     cy.get('.digit').contains('2').click();
@@ -59,7 +57,7 @@ describe('계산기 애플리케이션 테스트', () => {
 
     cy.get('#total').should('have.text', '123');
   })
-// [] 계산 결과를 표현할 때 소수점 이하는 버림한다.
+  
   it('계산 결과를 표현할 때 소수점 이하는 버림한다.', () => {
     cy.get('.digit').contains('1').click();
     cy.get('.operation').contains('/').click();
